3473a42468e2c04569a3a2309020cdb8ba7f8cbf
Dmytro Korol <dmytro.korol.ir.2021@lpnu.ua>
2022-06-02 20:37:01 +0300

    'AddCodeWithFixes'

--- Lab9.py
+++ Lab9.py
@@ -1,62 +1,62 @@
-class Animals:
-    def __init__(self, name, predatory, legs = 4, eyes = 2):
+class Animal:
+    def __init__(self, name: str, predatory: bool, legs: int = 4, eyes: int = 2):
         self.name = name
         self.predatory = predatory
         self.legs = legs
         self.eyes = eyes
 
-class Mammals(Animals):
-    def __init__(self, name, has_fur, predatory, legs = 4, eyes = 2):
-        super().__init__(name, legs, eyes, predatory)
+class Mammal(Animal):
+    def __init__(self, name: str, has_fur: bool , predatory: bool, legs: int = 4, eyes: int = 2):
+        super().__init__(name, predatory, legs, eyes)
         self.has_fur = has_fur
 
     def __str__(self):
-        return str(self.__class__) + ": " + str(self.__dict__)
+        return f"{str(self.__class__)}:  {str(self.__dict__)}"
 
-class Birds(Animals):
-    def __init__(self, has_feather, name, predatory, legs = 4, eyes = 2):
-        super().__init__(name, legs, eyes, predatory)
+class Bird(Animal):
+    def __init__(self, name: str, has_feather: bool , predatory: bool, legs: int = 4, eyes: int = 2):
+        super().__init__(name, predatory, legs, eyes)
         self.has_feather = has_feather
 
     def __str__(self):
-        return str(self.__class__) + ": " + str(self.__dict__)
+        return f"{str(self.__class__)}:  {str(self.__dict__)}"
 
-class Insects(Animals):
-    def __init__(self, can_fly, name, predatory, legs = 4, eyes = 2):
-        super().__init__(name, legs, eyes, predatory)
+class Insect(Animal):
+    def __init__(self, name: str, can_fly: bool, predatory: bool, legs: int = 4, eyes: int = 2):
+        super().__init__(name, predatory, legs, eyes)
         self.can_fly = can_fly
 
     def __str__(self):
-        return str(self.__class__) + ": " + str(self.__dict__)
+        return f"{str(self.__class__)}:  {str(self.__dict__)}"
 
-class Reptiles(Animals):
-    def __init__(self, can_swim, name, predatory, legs = 4, eyes = 2):
-        super().__init__(name, legs, eyes, predatory)
+class Reptile(Animal):
+    def __init__(self, name: str, can_swim: bool, predatory: bool, legs: int = 4, eyes: int = 2):
+        super().__init__(name, predatory, legs, eyes)
         self.can_swim = can_swim
 
     def __str__(self):
-        return str(self.__class__) + ": " + str(self.__dict__)
+        return f"{str(self.__class__)}:  {str(self.__dict__)}"
 
-class Fish(Animals):
-    def __init__(self, has_scales, name, predatory, legs = 4, eyes = 2):
-        super().__init__(name, legs, eyes, predatory)
+class Fish(Animal):
+    def __init__(self, name: str, has_scales: bool, predatory: bool, legs: int = 4, eyes: int = 2):
+        super().__init__(name, predatory, legs, eyes )
         self.has_scales = has_scales
 
     def __str__(self):
-        return str(self.__class__) + ": " + str(self.__dict__)
+        return f"{str(self.__class__)}:  {str(self.__dict__)}"
 
-mammal = Mammals(True, "wolf", True)
+mammal = Mammal("wolf", True,  True)
 print(mammal)
 
-bird = Birds(True, "Crow", True, 2)
+bird = Bird("Crow", True,  True, 2)
 print(bird)
 
-insect = Insects(True, "fly", True, 6, 6)
+insect = Insect("fly", True,  True, 6, 6)
 print(insect)
 
-reptile = Reptiles(True, "crocodile", True)
+reptile = Reptile("crocodile", True,  True)
 print(reptile)
 
-fish = Fish(True, "salmon",  False, 0)
+fish = Fish("salmon", True,  False, 0)
 print(fish)
 
